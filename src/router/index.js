fa78ca15f51292b30526955d3b6875db5776d1fb
v_chenshaoyi <d1vaainiyo@gmail.com>
2023-03-06 19:15:58 +0800

    update

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+import HelloWorldVue from "@/components/HelloWorld.vue";
+Vue.use(VueRouter);
+const routes = [
+  { path: "/foo", component: HelloWorldVue },
+  { path: "/", component: HelloWorldVue },
+];
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+export const router = new VueRouter({
+  routes, // (缩写) 相当于 routes: routes
+});
